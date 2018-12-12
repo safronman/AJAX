20105af2986c291e49f3c71ee26bec59cb176fb3
safronman <safronmanbox@gmail.com>
2018-12-12 10:02:29 +0300

    работа с get параметрами

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-$.ajax('https://repetitora.net/api/JS/Images', {
+let inputEl = document.querySelector('.ajax-input');
+let button = document.querySelector('.ajax-btn');
+
+button.addEventListener('click', makeRequest);
+
+function makeRequest() {
+  $.ajax(`https://repetitora.net/api/JS/Images?page=${inputEl.value}&count=1`, {
     success: function (data) {
       data.forEach(el => {
-        let content = document.querySelector('.content');
+        let content = document.querySelector('.response');
         let img = document.createElement('img');
         img.src = el.thumbnail;
         content.appendChild(img);
       });
     }
   });
+}
