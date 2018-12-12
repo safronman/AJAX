21e445cf54c71800370c562c7c18a4f69fb0b6e7
safronman <safronmanbox@gmail.com>
2018-12-12 15:53:19 +0300

    add axios

--- js/service.js
+++ js/service.js
@@ -1,7 +1,13 @@
-function getImages(pageNumber, succesCallback) {
-  $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
-    success: function (data) {
-      succesCallback(data);
-    }
+// Использоваие jQuery
+// function getImages(pageNumber) {
+//   let promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+//   return promise;
+// }
+
+// Использоваие axios
+function getImages(pageNumber) {
+  let promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+  return promise.then((response) => {
+    return response.data;
   });
 }
